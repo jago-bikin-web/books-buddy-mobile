b03b91b913825306f3c689ab37ed04db5458c0e7
fathonidf <daffafathoni@gmail.com>
2023-12-15 19:03:04 +0700

    feat: floating action button

--- lib/reachbuddy/screens/reachbuddy.dart
+++ lib/reachbuddy/screens/reachbuddy.dart
@@ -14,12 +14,27 @@ class ThreadsPage extends StatefulWidget {
   _ThreadsPageState createState() => _ThreadsPageState();
 }
 
-class _ThreadsPageState extends State<ThreadsPage> {
+class _ThreadsPageState extends State<ThreadsPage> with SingleTickerProviderStateMixin{
   List<Threads> _allThreads = [];
   bool _isLoading = true;
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
 
   String formatMonthDay(DateTime dateTime) {
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
     String month = monthNames[dateTime.month - 1];
     return "$month ${dateTime.day}";
   }
@@ -33,11 +48,31 @@ class _ThreadsPageState extends State<ThreadsPage> {
         _isLoading = false;
       });
     });
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    )..forward();
+    _animation = Tween<Offset>(
+      begin: const Offset(0.0, 0.5),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
   }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
+
   Future<List<Threads>> fetchThreads() async {
-    var url = Uri.parse('http://127.0.0.1:8000/reachbuddy/get-threads-flutter/');
-    var response = await http.get(url, headers: {"Content-Type": "application/json"});
+    var url =
+        Uri.parse('http://127.0.0.1:8000/reachbuddy/get-threads-flutter/');
+    var response =
+        await http.get(url, headers: {"Content-Type": "application/json"});
     var data = jsonDecode(utf8.decode(response.bodyBytes));
 
     List<Threads> listThreads = [];
@@ -102,82 +137,101 @@ class _ThreadsPageState extends State<ThreadsPage> {
           // Threads list
           Expanded(
             child: _isLoading
-              ? Center(child: CircularProgressIndicator())
-              : ListView.separated(
-                  itemCount: _allThreads.length,
-                  itemBuilder: (context, index) {
-                    Threads thread = _allThreads[index];
-                    String formattedDate = formatMonthDay(thread.date);
+                ? Center(child: CircularProgressIndicator())
+                : ListView.separated(
+                    itemCount: _allThreads.length,
+                    itemBuilder: (context, index) {
+                      Threads thread = _allThreads[index];
+                      String formattedDate = formatMonthDay(thread.date);
 
-                    return Card(
-                      elevation: 4,
-                      margin: EdgeInsets.all(8),
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(thread.bookImage, fit: BoxFit.cover),
-                            SizedBox(height: 10),
-                            Text(
-                              thread.bookTitle,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                            Text(
-                              'by ${thread.bookAuthor}',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 14,
-                              ),
-                            ),
-                            Divider(),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                CircleAvatar(
-                                  backgroundImage: NetworkImage(thread.profileImage),
+                      return Card(
+                        color: secondaryColour,
+                        elevation: 4,
+                        margin: EdgeInsets.all(8),
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.network(thread.bookImage,
+                                  fit: BoxFit.cover),
+                              SizedBox(height: 10),
+                              Text(
+                                thread.bookTitle,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
                                 ),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        thread.profileName,
-                                        style: TextStyle(fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        formattedDate,
-                                        style: TextStyle(color: Colors.grey),
-                                      ),
-                                      SizedBox(height: 10),
-                                      Text(thread.review),
-                                    ],
+                              ),
+                              Text(
+                                'by ${thread.bookAuthor}',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Divider(),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage:
+                                        NetworkImage(thread.profileImage),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Icon(Icons.thumb_up, size: 20),
-                                Text(' ${thread.likes}'),
-                              ],
-                            ),
-                          ],
+                                  SizedBox(width: 10),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          thread.profileName,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          formattedDate,
+                                          style: TextStyle(color: Colors.grey),
+                                        ),
+                                        SizedBox(height: 10),
+                                        Text(thread.review),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Icon(Icons.thumb_up, size: 20),
+                                  Text(' ${thread.likes}'),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => SizedBox(height: 0),
-                ),
+                      );
+                    },
+                    separatorBuilder: (context, index) => SizedBox(height: 0),
+                  ),
           ),
         ],
       ),
+      floatingActionButton: SlideTransition(
+        position: _animation,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 60.0),
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: primaryColour,
+            child: Icon(
+              Icons.edit_outlined,
+              color: Colors.white,
+              size: 40,
+            ),
+          ),
+        ),
+      ),
     );
   }
-
 }
