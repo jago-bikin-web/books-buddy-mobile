12d963897eb276461ac122cffd4f639904bc23b4
NadhiraWidyaniswari <nadhira.wiedyakusumo@gmail.com>
2023-12-15 23:48:38 +0700

    feat: eventbuddy

--- lib/eventbuddy/screens/event_list.dart
+++ lib/eventbuddy/screens/event_list.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:books_buddy/eventbuddy/models/event_models.dart';
+import 'package:books_buddy/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -15,7 +18,7 @@ class _EventPageState extends State<EventPage> {
   Future<List<Event>> fetchProduct() async {
     // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
     var url = Uri.parse(
-        'http://127.0.0.1:8000/eventbuddy/json/');
+        'http://127.0.0.1:8000/eventbuddy/get-event-flutter/');
     var response = await http.get(
         url,
         headers: {"Access-Control-Allow-Origin": "*",
@@ -42,6 +45,7 @@ Widget build(BuildContext context) {
         title: const Text('Event in Books Buddy'),
         ),
         //drawer: const LeftDrawer(),
+        backgroundColor: primaryColour,
         body: FutureBuilder(
             future: fetchProduct(),
             builder: (context, AsyncSnapshot snapshot) {
@@ -60,31 +64,50 @@ Widget build(BuildContext context) {
                         ],
                     );
                   } else {
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (_, index) => Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 12),
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                    Text(
-                                    "${snapshot.data![index].fields.name}",
-                                    style: const TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                    ),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                        "${snapshot.data![index].fields.description}")
-                                ],
-                                ),
-                            )
-                            );
-                    }
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) => Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: secondaryColour,  // Warna latar belakang kuning
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 10),
+                        Image.network(
+                          "${snapshot.data![index].bookThumbnail}",
+                          width: 100,  // Sesuaikan lebar gambar sesuai kebutuhan Anda
+                          height: 100,  // Sesuaikan tinggi gambar sesuai kebutuhan Anda
+                          fit: BoxFit.cover,  // Sesuaikan mode penyesuaian gambar
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          "${snapshot.data![index].eventName}",
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text("Date: ${snapshot.data![index].eventDate.toString().substring(0,10)}"),
+                        const SizedBox(height: 10),
+                        Text("${snapshot.data![index].eventDescription}"),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
                 }
             })
             );
--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: WelcomePage(),
+        home: Home(),
       ),
     );
   }
